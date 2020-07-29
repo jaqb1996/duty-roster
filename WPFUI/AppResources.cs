70e0e09c129b5ccb38445293b048463275465685
jaqb1996 <jaqb.klosinski@gmail.com>
2020-07-29 23:25:16 +0200

    Moved Schedule and DataAccess properties to the new class AppResources

---
+++ WPFUI/AppResources.cs
@@ -0,0 +1,23 @@
+﻿using ClassLibrary.DataAccess;
+using ClassLibrary.DataAccess.CSV;
+using ClassLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFUI
+{
+    public static class AppResources
+    {
+        public static ISchedule Schedule { get; set; }
+        public static IDataAccess DataAccess { get; }
+        static AppResources()
+        {
+            Schedule = null;
+            DataAccess = new CSVDataAccess();
+        }
+
+    }
+}
